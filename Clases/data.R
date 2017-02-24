e9449cc33fdfe17243621e04899e8f93ba8b986b
Michelle Sanchez <jemi2805@gmail.com>
2017-02-24 07:17:57 -0600

    Data R
    Clase del 23 de Febrero

---
+++ Clases/data.R
@@ -0,0 +1,5 @@
+x <-
+structure(1:3, .Names = c("foo", "bar", "bla"))
+y <-
+structure(list(a = 1, b = structure(1L, .Label = "a", class = "factor")), .Names = c("a", 
+"b"), row.names = c(NA, -1L), class = "data.frame")
